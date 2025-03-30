1a24a3a277b194578277a600a096fee75f6b2156
Marco Haubrich <marcolino.haubrich@gmail.com>
2025-03-30 15:45:56 -0300

    feat: dynamically determine the number of threads for incrementing the counter

--- projects/memoria_compartilhada_pthreads_rust/src/main.rs
+++ projects/memoria_compartilhada_pthreads_rust/src/main.rs
@@ -1,7 +1,6 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-const NUM_THREADS: usize = 2;
 const NUM_INCREMENTOS: i32 = 10000000;
 
 fn incrementa(contador: &Arc<Mutex<i32>>) {
@@ -15,9 +14,10 @@ fn main() {
     // Create mutex-protected counter and wrap in Arc for shared ownership
     let contador = Arc::new(Mutex::new(0));
     let mut handles = vec![];
+    let num_threads: usize = std::thread::available_parallelism().unwrap().get();
 
     // Spawn threads
-    for _ in 0..NUM_THREADS {
+    for _ in 0..num_threads {
         let contador_clone = Arc::clone(&contador);
         let handle = thread::spawn(move || {
             incrementa(&contador_clone);
@@ -33,7 +33,7 @@ fn main() {
     // Print results
     println!(
         "Valor final esperado do contador: {}",
-        NUM_THREADS as i32 * NUM_INCREMENTOS
+        num_threads as i32 * NUM_INCREMENTOS
     );
     println!("Valor final do contador: {}", *contador.lock().unwrap());
 }
